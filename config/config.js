d6fff6b14a7320e00738a1fcdff472e5185dcc9d
Andrew Sohn <andrewsohn@gmail.com>
2015-04-11 02:08:53 -0600

    new connection string

--- config/config.js
+++ config/config.js
@@ -2,7 +2,9 @@ var path = require('path'),
     rootPath = path.normalize(__dirname + '/..');
 
 //var db = "mongodb://access:wons@candidate.1.mongolayer.com:10127/avatech-production,mongodb://access:wons@candidate.1.mongolayer.com:10127,candidate.2.mongolayer.com:10127/avatech-production";
-var db = "mongodb://access:wons@candidate.34.mongolayer.com:10248,candidate.35.mongolayer.com:10248/avatech";
+//var db = "mongodb://access:wons@candidate.34.mongolayer.com:10248,candidate.35.mongolayer.com:10248/avatech";
+var db = "mongodb://access:wons@c248.candidate.34.mongolayer.com:10248,c248.candidate.35.mongolayer.com:10248/avatech?replicaSet=set-53e28c08f48cfdcd4f00030a";
+//var db = "mongodb://access:wons@c133.alcatraz.0.mongolayer.com:10133,c133.alcatraz.1.mongolayer.com:10133/avatech?replicaSet=set-5526f06c9c0d9210b9001d84";
 module.exports = {
     development: {
         db: db,

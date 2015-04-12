1863279271ca8bb5789623bf1932b86d4415612b
Andrew Sohn <andrewsohn@gmail.com>
2015-04-12 15:00:07 -0600

    remove build folder

--- _dist2/public/js/init.js
+++
@@ -1,7 +0,0 @@
-angular.element(document).ready(function() {
-    // fix facebook bug with redirect
-    if (window.location.hash == "#_=_") window.location.hash = "";
-
-    // init the app
-    angular.bootstrap(document, ['avatech']);
-});

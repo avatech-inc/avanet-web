9c20ac0f9f73bbb47f9cfd734e22613598354299
Andrew Sohn <andrewsohn@gmail.com>
2015-08-11 01:31:39 -0600

    cleanup

--- public/modules/map/route-planning.js
+++ public/modules/map/route-planning.js
@@ -661,26 +661,23 @@ angular.module('avatech').directive('routePlanning', function($http, $timeout, G
 
             elevationWidget = new ElevationWidget();
             elevationWidget.create(_map, {
-                position: "topright",
-                theme: "steelblue-theme", //default: lime-theme
                 imperial: Global.user.settings.elevation == 1, // true
                 //width: 670,
                 height: 165,
                 margins: {
                     top: 24,
-                    right: 20,
+                    right: 0,
                     bottom: 20,
                     left: 50
                 },
                 useHeightIndicator: true, //if false a marker is drawn at map position
-                interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
                 hoverNumber: {
                     decimalsX: 3, //decimals on distance (always in km)
                     decimalsY: 0, //deciamls on height (always in m)
                     formatter: undefined //custom formatter function may be injected
                 },
-                xTicks: undefined, //number of ticks in x axis, calculated by default according to width
-                yTicks: undefined, //number of ticks on y axis, calculated by default according to height
+                // xTicks: undefined, //number of ticks in x axis, calculated by default according to width
+                // yTicks: undefined, //number of ticks on y axis, calculated by default according to height
             });
         }
 

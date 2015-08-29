7529bd580597dc29c0726527ecc374e73e205d43
Andrew Sohn <andrewsohn@gmail.com>
2015-08-29 02:50:32 -0600

    cleanup

--- config/express.js
+++ config/express.js
@@ -77,23 +77,7 @@ module.exports = function(app) {
         res.status(500).send({ error: '500 Error' });
       } else {
         //next(err);
-        //res.status(500).sendFile('../app/views/500.html', { root: __dirname });
         res.status(500).sendFile(path.join(__dirname, '../app/views', '500.html'));
       }
     });
-
-
-    // //Assume "not found" in the error msgs is a 404. this is somewhat silly, but valid, you can do whatever you like, set properties, use instanceof etc.
-    // app.use(function(err, req, res, next) {
-    //     //Treat as 404
-    //     //if (~err.message.indexOf('not found')) return next();
-
-    //     //Log it
-    //     console.error(err.stack);
-
-    //     //Error page
-    //     res.status(500).render('500', {
-    //         error: err.stack
-    //     });
-    // });
 };

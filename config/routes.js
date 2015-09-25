96b37aa5bd011402a01456e06fba707b19926165
Andrew Sohn <andrewsohn@gmail.com>
2015-09-25 01:29:41 -0600

    cleanup

--- config/routes.js
+++ config/routes.js
@@ -3,10 +3,6 @@ var path = require('path');
 
 module.exports = function(app) {
 
-    // var obs = require('../app/controllers/observations');
-    // var profiles = require('../app/controllers/profiles');
-    // var tests = require('../app/controllers/tests');
-
     // NOT PORTED:
 
     // User
@@ -21,18 +17,6 @@ module.exports = function(app) {
     // and rename "observations" to "avalanches"?
     // app.get('/v1/all-observations', auth.requireLogin, profiles.all);
     // app.get('/v1/all-observations/mine', auth.requireLogin, profiles.allMine);
-
-    // Manual Profiles (JUST crud)
-    // app.post('/v1/profiles', auth.requireLogin, profiles.create);
-    // app.get('/v1/profiles/:profileId', auth.requireLogin, profiles.show);
-    // app.put('/v1/profiles/:profileId', auth.requireLogin, profiles.update);
-    // app.delete('/v1/profiles/:profileId', auth.requireLogin, profiles.destroy);
-
-    // Avalanche Observations (JUST crud)
-    // app.post('/v1/observations', auth.requireLogin, obs.create);
-    // app.get('/v1/observations/:observationId', auth.requireLogin, obs.show);
-    // app.put('/v1/observations/:observationId', auth.requireLogin, obs.update);
-    // app.delete('/v1/observations/:observationId', auth.requireLogin, obs.destroy);
 
     // Device Profiles
     // this handles both create and update
@@ -85,47 +69,37 @@ module.exports = function(app) {
     //--------------------------------------------------------------------------------
 
     // SMS
-
     var twilio = require('twilio')('AC90cc3e804675a5a3decaee1caac5f953', '92573d2ace3cea138517f2f76fc28689');
     app.get('/send-app-sms', function(req,res) {
-        console.log("hey!");
         console.log(req.query.num);
-
         twilio.sendMessage({
-
             to: req.query.num, // Any number Twilio can deliver to
             from: '+14355039000', // A number you bought from Twilio and can use for outbound communication
             body: 'Download the AvaNet app: https://avanet.avatech.com/app' // body of the SMS message
-
         }, function(err, responseData) { //this function is executed when a response is received from Twilio
-
             console.log(responseData);
             if (!err) { // "err" is an error received during the request, if any
-
                 // "responseData" is a JavaScript object containing data received from Twilio.
                 // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
                 // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-
                 // console.log(responseData.from); // outputs "+14506667788"
                 // console.log(responseData.body); // outputs "word"
-
             }
         });
-
         res.json({});
     });
     
-    app.get('/test500', function(req,res) { res.json(nonExistentVariable); });
+    //app.get('/test500', function(req,res) { res.json(nonExistentVariable); });
 
-    app.get('/manifest.plist', function(req,res) { 
-        res.sendFile(path.join(__dirname, '../app/views', 'manifest.plist'));
-    });
-    app.get('/download-app', function(req,res) { 
-        res.sendFile(path.join(__dirname, '../app/views', 'download-app.hyml'));
-    });
-    app.get('/app', function(req,res) { 
-        res.sendFile(path.join(__dirname, '../app/views', 'download-app.html'));
-    });
+    // app.get('/manifest.plist', function(req,res) { 
+    //     res.sendFile(path.join(__dirname, '../app/views', 'manifest.plist'));
+    // });
+    // app.get('/download-app', function(req,res) { 
+    //     res.sendFile(path.join(__dirname, '../app/views', 'download-app.hyml'));
+    // });
+    // app.get('/app', function(req,res) { 
+    //     res.sendFile(path.join(__dirname, '../app/views', 'download-app.html'));
+    // });
 
     var s3_2 = knox.createClient({
         key: 'AKIAIQFLR4EQC63ZZTNQ'
@@ -135,17 +109,13 @@ module.exports = function(app) {
 
     var getManual = function(req,res) {
         s3_2.list({ }, function(err, data) {
-
             var manual_en = "";
             var manual_fr = "";
-
             if (data.Contents && data.Contents.length > 0) {
-
                 for (var i = 0; i < data.Contents.length; i++) {
                     if (data.Contents[i].Key.toLowerCase().indexOf("english") > -1) manual_en = data.Contents[i].Key;
                     else if (data.Contents[i].Key.toLowerCase().indexOf("french") > -1) manual_fr = data.Contents[i].Key;
                 }
-
                 if (!req.query.l|| (manual_en != "" && req.query.l.toLowerCase() == "en")) {
                     return res.redirect("https://s3.amazonaws.com/sp1manual/" + manual_en);
                 }
@@ -161,9 +131,6 @@ module.exports = function(app) {
     app.get('/manual', getManual);
 
     app.get('*', function(req,res) { 
-
-        //console.log("ACCEPT HEADERS: " + req.headers.accept);
-
         res.sendFile(path.join(__dirname, '../app/views', 'main.html'));
     });
 };

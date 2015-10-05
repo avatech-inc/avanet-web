bd23ef040bde05dffc2397cda6a86709aed2b536
Andrew Sohn <andrewsohn@gmail.com>
2015-10-05 16:24:12 -0600

    move scale canvas drawing into own file

--- public/modules/map/route-planning.js
+++ public/modules/map/route-planning.js
@@ -109,152 +109,14 @@ angular.module('avatech').directive('routePlanning', function($http, $timeout, G
             dpi = null;
             return dpi_x;
         }
-        scope.getScale = function() {
+        scope.getMetersPerPixel = function() {
             var center = scope.map.getCenter();
             var bounds = scope.map.getBounds();
 
             var distance = new L.LatLng(center.lat, bounds._southWest.lng).distanceTo(new L.LatLng(center.lat, bounds._northEast.lng));
             var metersPerPixel = distance / scope.map.getSize().x;
-            var milesPerPixel = metersPerPixel * 0.000621371;
-
-            //var pixelsPerMeter = scope.map.getSize().x / distance;
-            console.log("map width: " + scope.map.getSize().x)
-            console.log("map distance: " + distance + " m");
-            console.log("m/pixel: " + metersPerPixel);
-            console.log("DPI: " + scope.getDPI());
-            var inchesPerMeter = 39.3701;
-            var mapScale = Math.round(inchesPerMeter * metersPerPixel * scope.getDPI());
-            console.log("MAP SCALE: 1:" + mapScale)
-
-            drawCanvas(true);
-            drawCanvas(false);
-
-            function drawCanvas(metric) {
-                var scaleWidth = 420;
-                var marginLeft = 30;
-                var marginRight = 30;
-                
-                var canvas = document.createElement("canvas");
-                canvas.width = (scaleWidth + marginLeft + marginRight) * 2;
-                canvas.height = 36 * 2;
-
-                var ctx = canvas.getContext('2d');
-                ctx.scale(2,2);
-                ctx.fillStyle = "white";
-                ctx.fillRect(0,0,canvas.width / 2, canvas.height / 2);
-
-                var topY = 20;
-                var scaleBarHeight = 6;
-
-                // calculate scale bar sections
-                var units, totalUnits;
-                    
-                // metric (km and m)
-                if (metric) {
-                    // how many meters can we fit?
-                    var scaleMeters = scaleWidth * metersPerPixel;
-
-                    // km
-                    if (scaleMeters > 1000 && scaleMeters <= 2000) units = 500; // .5 km
-                    else if (scaleMeters > 4000000) units = 1000000; // 1000km 
-                    else if (scaleMeters > 1000000) units = 500000; // 500km
-                    else if (scaleMeters > 400000) units = 200000; // 200km
-                    else if (scaleMeters > 200000) units = 100000; // 100km
-                    else if (scaleMeters > 160000) units = 50000; // 40km
-                    else if (scaleMeters > 80000) units = 20000; // 20km
-                    else if (scaleMeters > 40000) units = 10000; // 10km
-                    else if (scaleMeters > 20000) units = 5000; // 5km
-                    else if (scaleMeters > 10000) units = 2000; // 2km
-                    else if (scaleMeters > 1000) units = 1000; // 1 km
-                    // meters
-                    else if (scaleMeters > 200) units = 100; // 100 m
-                    else units = 50; // 50m
-
-                    totalUnits = Math.floor(scaleMeters / units);
-                }
-                // imperial (miles)
-                else {
-                    // how many miles can we fit?
-                    var scaleMiles = scaleWidth * milesPerPixel;
-
-                    if (scaleMiles <= .2) units = .05;
-                    else if (scaleMiles <= .5) units = .1;
-                    else if (scaleMiles <= 1) units = .2;
-                    else if (scaleMiles < 3) units = .5;
-                    else if (scaleMiles <= 7) units = 1;
-                    else if (scaleMiles <= 10) units = 2;
-                    else if (scaleMiles <= 20) units = 5;
-                    else if (scaleMiles <= 50) units = 10;
-                    else if (scaleMiles <= 100) units = 20;
-                    else if (scaleMiles <= 250) units = 50;
-                    else if (scaleMiles <= 400) units = 100;
-                    else if (scaleMiles <= 1000) units = 200;
-                    else if (scaleMiles <= 3000) units = 500;
-                    else units = 1000;
-
-                    totalUnits = Math.floor(scaleMiles / units);
-                }
-
-                ctx.lineWidth = 1;
-                ctx.strokeStyle = "black";
-
-                // draw label
-                ctx.fillStyle = "black";
-                ctx.font = "10px Arial";
-                var labelText = "0";
-                ctx.fillText(labelText, marginLeft - (ctx.measureText(labelText).width / 2), topY - 4);
-
-                //var totalUnits = max / units;
-                for (var i = 1; i <= totalUnits; i++) {
-
-                    var left_x, right_x;
-                    if (metric) {
-                        var pixelsPerKm = units / metersPerPixel;
-                        var left_x = pixelsPerKm * (i - 1);
-                        var right_x = pixelsPerKm * i;
-                    }
-                    else {
-                        var pixelsPerMile = units / milesPerPixel;
-                        var left_x = pixelsPerMile * (i - 1);
-                        var right_x = pixelsPerMile * i;
-                    }
-
-                    // draw label
-                    ctx.fillStyle = "black";
-                    ctx.font = "10px Arial";
-
-                    var labelText, unitsText;
-                    if (metric) {
-                        labelText = units >= 500 ? i * (units / 1000) : i * units;
-                        unitsText = units <= 100 ? "m" : "km"
-                    }
-                    else {
-                        var labelNum = i * units;
-                        labelText = labelNum % 1 != 0 ? labelNum.toFixed(1) : labelNum;
-                        unitsText = "mi";
-                    }
-                    ctx.fillText(i < totalUnits ? labelText : labelText + " " + unitsText, right_x + marginLeft - (ctx.measureText(labelText).width / 2), topY - 4);
-
-                    // draw box
-                    ctx.beginPath();
-                    ctx.moveTo(left_x + marginLeft, topY);
-                    ctx.lineTo(right_x + marginLeft, topY);
-                    ctx.lineTo(right_x + marginLeft, topY + scaleBarHeight);
-                    ctx.lineTo(left_x + marginLeft, topY + scaleBarHeight);
-                    ctx.lineTo(left_x + marginLeft, topY);
-
-                    // draw center line
-                    if ((i + 1) % 2 == 0) {
-                        ctx.moveTo(marginLeft + left_x, topY + (scaleBarHeight / 2));
-                        ctx.lineTo(marginLeft + right_x, topY + (scaleBarHeight / 2));
-                    }
-                    ctx.stroke();
-                }
-
-                document.body.appendChild(canvas);
-                if (metric) canvas.setAttribute("style","position: absolute; top:0;left:0;z-index:999999;width:" + (canvas.width / 2) + "px;height:" + (canvas.height / 2) + "px");
-                else if (!metric) canvas.setAttribute("style","position: absolute; top:36px;left:0;z-index:999999;width:" + (canvas.width / 2) + "px;height:" + (canvas.height / 2) + "px");
-            }
+            
+           return metersPerPixel;
         }   
         scope.control.downloadPDF = function() {
             var pdfRows = [];
@@ -352,7 +214,15 @@ angular.module('avatech').directive('routePlanning', function($http, $timeout, G
             // get scale
             // calculate distance from left to right of map in pixels
 
-            scope.getScale();
+            var metersPerPixel = scope.getMetersPerPixel();
+
+             // console.log("DPI: " + scope.getDPI());
+            // var inchesPerMeter = 39.3701;
+            // var mapScale = Math.round(inchesPerMeter * metersPerPixel * scope.getDPI());
+            // console.log("MAP SCALE: 1:" + mapScale)
+
+            var scale_canvas = DrawScaleCanvas(metersPerPixel);//, true);
+            //DrawScaleCanvas(metersPerPixel, false);
 
             //pdfMake.createPdf(docDefinition).open();
             // .download()
@@ -370,7 +240,7 @@ angular.module('avatech').directive('routePlanning', function($http, $timeout, G
                 });
                  docDefinition.content.push({
                     image: scale_canvas.toDataURL('image/jpeg',1),
-                    width: 300,
+                    width: (scale_canvas.width / 2), // * .635
                 });
                 pdfMake.createPdf(docDefinition).download();
 

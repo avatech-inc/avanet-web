14ce6ce05d73243bfd1d96e9614f19913cf3b750
Andrew Sohn <andrewsohn@gmail.com>
2015-11-02 21:05:39 -0700

    bind points

--- public/modules/map/rose-graph.js
+++ public/modules/map/rose-graph.js
@@ -3,6 +3,7 @@ angular.module('avatech').directive('roseGraph', function($timeout) {
   return {
     restrict: 'A',
     scope: { 
+        points: '@roseGraph' // must use @ instead of = because of angular filter
     },
     link: function(scope, element) {
     }

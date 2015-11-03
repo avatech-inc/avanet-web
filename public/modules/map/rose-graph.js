ff880199bcc2f12cb281fe830909e8af6027553e
Andrew Sohn <andrewsohn@gmail.com>
2015-11-02 21:06:01 -0700

    watch points

--- public/modules/map/rose-graph.js
+++ public/modules/map/rose-graph.js
@@ -6,6 +6,11 @@ angular.module('avatech').directive('roseGraph', function($timeout) {
         points: '@roseGraph' // must use @ instead of = because of angular filter
     },
     link: function(scope, element) {
+        var timer;
+        scope.$watch('points', function() {
+            if (timer) $timeout.cancel(timer);
+            timer = $timeout(render, 300);
+        });
     }
   };
 });

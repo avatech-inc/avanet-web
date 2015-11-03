d64914721deb13159b71c33527c1d3651bd4e044
Andrew Sohn <andrewsohn@gmail.com>
2015-11-02 21:05:13 -0700

    init directive

--- public/modules/map/rose-graph.js
+++ public/modules/map/rose-graph.js
@@ -1,3 +1,10 @@
 angular.module('avatech').directive('roseGraph', function($timeout) {
   
+  return {
+    restrict: 'A',
+    scope: { 
+    },
+    link: function(scope, element) {
+    }
+  };
 });

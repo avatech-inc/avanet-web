e82f311dca344d45b80c7b1c3979f90879e6e1a3
Andrew Sohn <andrewsohn@gmail.com>
2015-11-05 16:27:33 -0700

    remove [] annotations

--- public/modules/admin/admin-users.js
+++ public/modules/admin/admin-users.js
@@ -1,4 +1,4 @@
-angular.module('avatech.system').controller('AdminUsersController', ['$scope', '$location', '$http', 'Global', 'Users', function ($scope, $location, $http, Global, Users) {
+angular.module('avatech.system').controller('AdminUsersController', function ($scope, $location, $http, Global, Restangular) {
     $scope.global = Global;
 
     // executes on 'page load'
@@ -216,4 +216,4 @@ angular.module('avatech.system').controller('AdminUsersController', ['$scope', '
         });
     }
 
-}]);
+});

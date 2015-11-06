9a082909da0c565b73eb8c45cd2e7990e47405f2
Andrew Sohn <andrewsohn@gmail.com>
2015-11-06 14:49:07 -0700

    extreme commenting

--- public/js/init.js
+++ public/js/init.js
@@ -15,16 +15,24 @@ angular.element(document).ready(function() {
     if (auth) {
     	$http.defaults.headers.common['Auth-Token'] = auth.authToken;
     	$http.get(window.apiBaseUrl + 'users/' + auth.userId)
-    	.then(function(response) {
+    	.then(
+        // user found
+        function(response) {
     		window._user = response.data;
             angular.bootstrap(document, ['avatech']);
     	},
+        // error
         function(response) {
             // if API is not found, show API error
+            // why don't we do this through angular? 
+            // 1) it would be a sizeable amount of hacky code just for this (as opposed to one line here)
+            // 2) it would require the entire app to be boostrapped just to show an error (slow)
+            // 3) there's nothing interactive that needs to happen on the error page - purely static
+            // todo: well worth revisiting in the future...
             if (response.status == 0 || response.status == 500) 
                 document.body.className += ' api-error';
-            
-            // if 404, start the app (go to login page)
+
+            // if 40X, start the app regularly (goes to login page)
             else angular.bootstrap(document, ['avatech']);
         });
     }

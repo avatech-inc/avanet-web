6bc6df317f04b964066a40909efc42b8b9d6f4c7
Andrew Sohn <andrewsohn@gmail.com>
2015-11-08 18:03:20 -0700

    remove source map

--- public/js/lib/PruneCluster.js
+++ public/js/lib/PruneCluster.js
@@ -919,4 +919,3 @@ var PruneClusterLeafletSpiderfier = (L.Layer ? L.Layer : L.Class).extend({
         map.off('zoomend', this.Unspiderfy, this);
     }
 });
-//# sourceMappingURL=PruneCluster.js.map

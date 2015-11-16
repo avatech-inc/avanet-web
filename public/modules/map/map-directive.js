fefaf942271cbed9375df4426061aa350d7b5318
Andrew Sohn <andrewsohn@gmail.com>
2015-11-16 12:11:11 -0700

    .promise isn't needed

--- public/modules/map/map-directive.js
+++ public/modules/map/map-directive.js
@@ -367,7 +367,7 @@ angular.module('avatech').directive('map', function($timeout, $q, $rootScope, $t
 
     // set base layer after map has been initialized and layers have been loaded from server
     $q.all([
-        mapLoaded.promise,
+        mapLoaded,
         scope.mapLayers.loaded
     ]).then(function() {
 

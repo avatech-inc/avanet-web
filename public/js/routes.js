058d06ebf64de2c1afe39d5adb7ef6e4e1160bb1
Andrew Sohn <andrewsohn@gmail.com>
2015-11-18 08:01:12 -0700

    SP1 -> SP

--- public/js/routes.js
+++ public/js/routes.js
@@ -17,15 +17,7 @@ angular.module('avatech').config(['$routeProvider','$stateProvider', '$urlRouter
         .state('index.route', {
             url: 'routes/:routeId',
             views: { "route-pane": { templateUrl: '/modules/map/route-planning.html' } },
-            //reload: true,
-            //sticky: true,
-            data: {
-                //showPreviewPane: true
-            }
-            // data: {
-            //     title: 'AvaTech',
-            //     requireLogin: true 
-            // }
+            data: { }
         })
 
         // .state('index.newObservation', {
@@ -84,7 +76,7 @@ angular.module('avatech').config(['$routeProvider','$stateProvider', '$urlRouter
         //     }
         // })
 
-        // // Snowpit Editor
+        // Snowpit Editor
         .state('index.profileEditor', {
             abstract: true,
             //url: '/profiles/:profileId',
@@ -148,19 +140,19 @@ angular.module('avatech').config(['$routeProvider','$stateProvider', '$urlRouter
         .state('index.SP1update2', {
             url: 'sp1update',
             views: { "content": { templateUrl: '/modules/firmware-update/SP1update.html' } },
-            data: { 
-                title: 'SP1 Firmware Update / Avanet',
-                requireLogin: true,
-                fullScreen: true
-            }
+            data: { title: 'SP Firmware Update / Avanet', requireLogin: true, fullScreen: true }
         }).state('index.SP1update', {
             url: 'SP1update',
             views: { "content": { templateUrl: '/modules/firmware-update/SP1update.html' } },
-            data: { 
-                title: 'SP1 Firmware Update / Avanet',
-                requireLogin: true,
-                fullScreen: true
-            }
+            data: { title: 'SP Firmware Update / Avanet', requireLogin: true, fullScreen: true }
+        }).state('index.SPupdate', {
+            url: 'SPupdate',
+            views: { "content": { templateUrl: '/modules/firmware-update/SP1update.html' } },
+            data: { title: 'SP Firmware Update / Avanet', requireLogin: true, fullScreen: true }
+        }).state('index.SPupdate1', {
+            url: 'spupdate',
+            views: { "content": { templateUrl: '/modules/firmware-update/SP1update.html' } },
+            data: { title: 'SP Firmware Update / Avanet', requireLogin: true, fullScreen: true }
         })
 
         .state('index.support', {

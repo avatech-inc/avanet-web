d9e739b2c0fe98994d58fad8391dc44b34af1158
Andrew Sohn <andrewsohn@gmail.com>
2015-11-23 23:43:59 -0700

    js source maps in build

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,13 @@
 var gulp = require('gulp');
-var path = require('path');
 var fs = require("fs");
 var nodemon = require('gulp-nodemon');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
 var inject = require('gulp-inject');
-var useref = require('gulp-useref');
 var gulpif = require('gulp-if');
 var uglify = require('gulp-uglify');
 var runSequence = require('run-sequence');
 var rev = require('gulp-rev');
-var revReplace = require('gulp-rev-replace');
 var clean = require('gulp-clean');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
@@ -19,11 +16,13 @@ var ngAnnotate = require('gulp-ng-annotate');
 var replace = require('gulp-replace-task');
 var gutil = require('gulp-util');
 var argv = require('yargs').argv;
-//var rollbar = require('gulp-rollbar');
-var stripDebug = require('gulp-strip-debug');
-var rename = require("gulp-rename");
 var htmlmin = require('gulp-htmlmin');
 var insert = require('gulp-insert');
+var concat = require('gulp-concat');
+var htmlreplace = require('gulp-html-replace');
+var bump = require('gulp-bump');
+var tap = require('gulp-tap');
+//var stripDebug = require('gulp-strip-debug');
 
 var s3 = require("gulp-s3");
 var aws = {
@@ -31,6 +30,144 @@ var aws = {
   , secret: 'VgUlNeUB02Q0pgsflT+p3/vDY9LdsXLOx/4IIONk'
   , bucket: 'avanet'
 };
+
+// ----------------------------------------------------------------------------------
+
+  var bundleName = "avanet";
+  var releaseVersion;
+
+  var _sources = [
+    // libraries
+
+    // Leaflet
+    'public/js/lib/leaflet.js',
+    // Leaflet Draw
+    'public/js/lib/leaflet.draw-src.js',
+    // PNG processing
+    'public/js/lib/zlib.js', 
+    'public/js/lib/png.js', 
+    // UTM-lat/lng conversion
+    'public/js/lib/utm.js', 
+    // great circle math
+    'public/js/lib/greatCircle.js', 
+    // world magnetic model
+    'public/js/lib/WorldMagneticModel.js', 
+    // DrawDeclinationCanvas
+    //'public/js/lib/DrawDeclinationCanvas.js', 
+    // DrawScaleCanvas
+    //'public/js/lib/DrawScaleCanvas.js', 
+    // underscore
+    'public/lib/underscore/underscore.js', 
+    // Leaflet PruneCluster plugin
+    'public/js/lib/PruneCluster.js',
+    // Leaflet Heatmap plugin
+    'public/js/lib/leaflet-heat.js',
+    // Leaflet Image Export plugin
+    'public/js/lib/leaflet-image.js',
+    // Leaflet Canvas Layer plugin
+    'public/js/lib/leaflet_canvas_layer.js',
+    // turf.js
+    'public/js/lib/turf.js',
+    // simplify.js
+    'public/js/lib/simplify.js',
+    // Leaflet Graticule
+    'public/js/lib/graticule.js',
+    // Angular
+    'public/lib/angular/angular.js',
+    'public/lib/angular-cookies/angular-cookies.js',
+    'public/lib/angular-route/angular-route.js',
+    'public/lib/angular-touch/angular-touch.js',
+    // Restangular
+    'public/lib/restangular/dist/restangular.js',
+    // angular forms
+    'public/lib/objectpath/lib/ObjectPath.js',
+    'public/lib/tv4/tv4.js',
+    'public/lib/angular-sanitize/angular-sanitize.js',
+    'public/lib/angular-schema-form/dist/schema-form.js',
+    'public/lib/angular-schema-form/dist/bootstrap-decorator.js',
+    // bindonce
+    'public/lib/angular-bindonce/bindonce.js',
+    // mentions input
+    'public/js/lib/jquery.mentionsInput.js',
+    'public/js/lib/jquery.elastic.js',
+    // ment.io
+    'public/lib/ment.io/dist/mentio.js', // <--- still used?
+    // suncalc
+    //'public/js/lib/suncalc.js',
+    // moment.js time formatter
+    'public/lib/moment/moment.js',
+    // angular wrapper for moment.js
+    'public/lib/angular-moment/angular-moment.js',
+    // jQuery Knob
+    'public/js/lib/jquery.knob.js',
+    // md5
+    'public/lib/blueimp-md5/js/md5.js',
+    // jsPDF
+    'public/lib/jspdf/dist/jspdf.debug.js',
+    // pdfmake
+    'public/lib/pdfmake/build/pdfmake.js',
+    'public/lib/pdfmake/build/vfs_fonts.js',
+    // Angular local storage
+    'public/lib/angular-local-storage/angular-local-storage.js',
+    // Angular country picker
+    'public/lib/angular-country-picker/country-picker.js',
+    // Angular Translate
+    'public/lib/angular-translate/angular-translate.js',
+    'public/lib/angular-translate-loader-partial/angular-translate-loader-partial.js',
+    // Angular UI Router
+    'public/lib/angular-ui-router/release/angular-ui-router.js',
+    'public/lib/angular-ui-utils/modules/route/route.js',
+    // Angular UI Router Extras
+    'public/lib/ui-router-extras/release/ct-ui-router-extras.js',
+    // Angular UI Bootstrap
+    'public/js/lib/ui-bootstrap-0.14.3.js',
+    'public/js/lib/ui-bootstrap-tpls-0.14.3.js',
+    // Angular CC forms
+    'public/lib/angular-credit-cards/release/angular-credit-cards.js',
+    // Venturocket UI slider control
+    'public/lib/venturocket-angular-slider/build/angular-slider.js',
+    // regular ol' Bootsrap JS for tooltips
+    'public/js/lib/bootstrap.js',
+    // Lightbox
+    'public/lib/angular-loading-bar/build/loading-bar.js',
+    'public/lib/angular-bootstrap-lightbox/dist/angular-bootstrap-lightbox.js',
+    'public/lib/angular-bootstrap-lightbox/dist/angular-bootstrap-lightbox.css',
+    // elevation profile
+    'public/js/lib/elevation-widget.js',
+    // color picker
+    'public/lib/ngjs-color-picker/js/ngjs-color-picker.js',
+    'public/lib/ngjs-color-picker/css/ngjs-color-picker.css',
+    // Angular Audio
+    'public/lib/angular-audio/app/angular.audio.js',
+    // FontLoader
+    'public/js/lib/FontLoader.js',
+    // nanoscroller
+    'public/lib/nanoscroller/bin/javascripts/jquery.nanoscroller.js',
+    'public/lib/angular-nanoscroller/scrollable.js',
+    // pikaday
+    'public/lib/pikaday/pikaday.js',
+    'public/lib/pikaday-angular/pikaday-angular.js',
+    // FileSaver
+    'public/js/lib/FileSaver.js',
+    // canvas.toBlob()
+    'public/js/lib/canvas-toBlob.js',
+
+    // Avanet App
+    'public/js/app.js',
+    'public/js/routes.js',
+    // app scripts
+    'public/modules/**/*.js',
+    'public/js/services/*.js',
+    'public/js/controllers/*.js',
+    'public/js/controllers/*/*.js',
+    'public/js/directives/*.js',
+    'public/js/directives/*/*.js',
+    'public/css/**/*.css',
+    // init
+    'public/js/init.js',
+  ];
+
+// ----------------------------------------------------------------------------------
 
 gulp.task('upload-s3', function() {
   return gulp.src('server/views/503-maint.html', { read: true})
@@ -58,79 +195,85 @@ gulp.task('compass', ['clean-css'], function() {
   .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('ng-annotate', function() {
-  return gulp.src(['_dist/public/assets/*.js'])
-  .pipe(ngAnnotate())
-  .pipe(gulp.dest(function(file) { return file.base; }));
+// gulp.task('copyright', function() {
+//   return gulp.src(['_dist/public/assets/*.js'])
+//   .pipe(insert.prepend('// Copyright (C) 2015 Avatech, Inc.\n'))
+//   .pipe(insert.append(';(function(){setInterval(function(){console.log("Copyright (C) 2015 Avatech, Inc.")},10000);})();'))
+//   .pipe(gulp.dest(function(file) { return file.base; }));
+// });
+
+gulp.task('sentry', function() {
+  var opt = {
+    DOMAIN: '', // prefix domain in the `name` param when upload file. Leave blank to use path. Do not add trailing slash 
+    API_URL: 'https://app.getsentry.com/api/0/projects/avatech/web-client/',
+    API_KEY: 'c8d2219c66e54cb1bfe5142927bbc116',
+    debug: false ,
+    versionPrefix: '', // Append before the version number in package.json 
+  }
+  var sentryRelease = require('gulp-sentry-release')('./package.json', opt);
+
+  return gulp.src(_sources, { base: 'public' })
+  .pipe(sentryRelease.release());
 });
 
-gulp.task('copyright', function() {
-  return gulp.src(['_dist/public/assets/*.js'])
-  .pipe(insert.prepend('// Copyright (C) 2015 Avatech, Inc.\n'))
-  .pipe(insert.append(';(function(){setInterval(function(){console.log("Copyright (C) 2015 Avatech, Inc.")},10000);})();'))
-  .pipe(gulp.dest(function(file) { return file.base; }));
-});
-
-gulp.task('strip-debug', function() {
-  return gulp.src([
-    '_dist/public/js/**/*.js',
-    '_dist/public/modules/**/*.js',
-    //'!_dist/public/js/lib',
-  ])
-  .pipe(stripDebug())
-  .pipe(gulp.dest(function(file) { return file.base; }));
-});
-
-gulp.task('combine', function() {
-
-    return gulp.src('_dist/server/views/main.html')
-    .pipe(replace({
-        patterns: [{ match: 'env', replacement: 'production' }]
-    }))
-
-    .pipe(useref({ searchPath: '_dist/public' }))
+var revHashes = {};
+gulp.task('combine-minify', function() {
+  return gulp.src(_sources, { base: 'public' })
+    // start creating source map
+    .pipe(sourcemaps.init())
+    // combine files
+    .pipe(gulpif("*.js", concat({ path: bundleName + '.js', cwd: '' })))
+    .pipe(gulpif("*.css", concat({ path: bundleName + '.css', cwd: '' })))
+    // rename the concatenated files for cache-busting
+    .pipe(rev())
 
     // minify CSS
     .pipe(gulpif('*.css', minifyCSS({ keepSpecialComments: 0 })))
 
-    // rename the concatenated files for cache-busting
-    .pipe(rev())             
-    // substitute in new filenames in main.html
-    .pipe(revReplace())      
+    // add angular [] annotations
+    .pipe(gulpif("*.js", ngAnnotate({ gulpWarnings: false })))
 
-    // sourcemaps
-    // .pipe(rollbar({
-    //   accessToken: '887db6d480c74bab9cee4ab6376f1545',
-    //   version: 'version_test',
-    //   sourceMappingURLPrefix: 'https://avanet.avatech.com'
-    // }))
-    //.pipe(gulpif('*.js', sourcemaps.write('./')))
+    // strip debug statements
+    //.pipe(gulpif("*.js", stripDebug()))
+
+    // uglify JS
+    .pipe(gulpif("*.js", uglify()))
+
+    // write js source map
+    .pipe(gulpif("*.js", sourcemaps.write(".", { includeContent: true })))
 
     // add all assets to dist folder
-    .pipe(gulpif('*.css', gulp.dest('_dist/public')))
-    .pipe(gulpif('*.js', gulp.dest('_dist/public')))
-    //.pipe(gulpif('*.map', gulp.dest('_dist/public')))
+    .pipe(gulpif('*.css', gulp.dest('_dist/public/assets')))
+    .pipe(gulpif('*.js', gulp.dest('_dist/public/assets')))
+    .pipe(gulpif('*.map', gulp.dest('_dist/public/assets')))
 
-    .pipe(gulpif('*.html', rename("main.html")))
-    .pipe(gulpif('*.html', gulp.dest('_dist/server/views')))
+    // store rev file hashes for use later
+    .pipe(tap(function(file) {
+      if (file.revHash) revHashes[file.revOrigPath] = file.revHash;    
+  }))
 });
 
-gulp.task('uglify', function() {
-  return gulp.src(['_dist/public/assets/*.js'])
-  .pipe(uglify())
-  .pipe(gulp.dest('_dist/public/assets'));
-});
-
-gulp.task('clean-html', function() {
+gulp.task('clean-main', function() {
   return gulp.src('_dist/server/views/main.html')
+    // replace variable
+    .pipe(replace({
+        patterns: [
+          { match: 'env', replacement: 'production' },
+          { match: 'release', replacement: releaseVersion }
+        ]
+    }))
+    // replace js files with combined files (include rev hash)
+    .pipe(htmlreplace({ 
+      js:  '/assets/' + bundleName + '-' + revHashes[bundleName + '.js']  + '.js', 
+      css: '/assets/' + bundleName + '-' + revHashes[bundleName + '.css'] + '.css' 
+    }))
+    // clean html
     .pipe(htmlmin({ 
       removeComments: true,
       collapseWhitespace: true,
       preserveLineBreaks: true
     }))
-    .pipe(gulp.dest(function(file) {
-      return file.base;
-    }));
+    .pipe(gulp.dest(function(file) { return file.base; }));
 });
 
 gulp.task('clean-dist', function() {
@@ -142,28 +285,15 @@ gulp.task('clean-dist', function() {
     '_dist/public/sass',
     ], { read: false })
       .pipe(clean({ force: true }));
-})
+});
 
 gulp.task('buildMain', function () {
-  var target = gulp.src('server/views/main.html');
-
-  var sources = gulp.src([
-	  'public/modules/**/*.js',
-    'public/js/services/*.js',
-    'public/js/controllers/*.js',
-    'public/js/controllers/*/*.js',
-    'public/js/directives/*.js',
-    'public/js/directives/*/*.js',
-    'public/css/**/*.css',
-  ],
   // don't read the file contents - we only need filenames
-  {read: false});
- 
-  return target.pipe(inject(sources, {
-        ignorePath: 'public/',
-        addRootSlash: true,
-    }))
-    .pipe(gulp.dest('server/views'));
+  var sources = gulp.src(_sources, { read: false });
+  // inject into main.html
+  return gulp.src('server/views/main.html')
+  .pipe(inject(sources, { ignorePath: 'public/', addRootSlash: true }))
+  .pipe(gulp.dest('server/views'));
 });
 
 gulp.task('clean', function() {
@@ -271,29 +401,37 @@ gulp.task('deploy', function(done){
 	});
 });
 
+gulp.task('bump', function(){
+  gulp.src('package.json')
+  .pipe(bump({ type: 'patch' }))
+  // keep track of new version
+  .pipe(tap(function(file){
+      var json = JSON.parse(String(file.contents));
+      releaseVersion = json.version;
+  }))
+  .pipe(gulp.dest(function(file) { return file.base; }));
+});
+
 gulp.task('build', function(done) {
   runSequence('compass',
+    // bump version
+    'bump',
     // inject files into main
     'buildMain',
     // remove existing _dist folder
     'clean', 
-    // copy files into _Dist
+    // copy files into _dist
     'copy',
-    // strip out 'console.log' etc.
-    'strip-debug',
-    // concat files
-    'combine', 
-    // add angular [] annotations
-    'ng-annotate',
+    //'sentry',
+    // combine and minify js and css
+    'combine-minify',
     // clean main.html
-    'clean-html',
-    // uglify javascript
-    'uglify',
-    // add copyright statements to app.js
-    'copyright',
+    'clean-main',
+    // // add copyright statements to app.js
+    // 'copyright',
     // cleanup files
     'clean-dist', 
-    // prepare git for push to heroku
+    // prepare git for pushing to heroku
     'git',
   done);
 });

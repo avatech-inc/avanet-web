c9c67304fb96e8edd944101fada5844297b8c217
Andrew Sohn <andrewsohn@gmail.com>
2015-11-24 22:25:05 -0700

    cleanup

--- public/modules/user/settings.js
+++ public/modules/user/settings.js
@@ -59,9 +59,7 @@ function ($scope, $q, $stateParams, $location, $timeout, Global, Restangular, $h
         });
     }
 
-    
     // LOCATION LAT/LNG INPUT
-
     $scope.$watch('user.location', function(newLocation, oldLocation) {
         if (!oldLocation || !newLocation) return;
         
@@ -72,118 +70,17 @@ function ($scope, $q, $stateParams, $location, $timeout, Global, Restangular, $h
             $scope.global.setUser($scope.user);
         }
     });
-    // $scope.$watch('userLocation._lat', function(newLat) {
-    //     if (!$scope.obs) return;
-    //     if (!newLat) return;
-    //     newLat = parseFloat(newLat);
-    //     if (isNaN(newLat)) newLat = 0;
-    //     if (newLat > 90) newLat = 90;
-    //     else if (newLat < -90) newLat = -90;
-    //     if (!$scope.userLocation.location) $scope.userLocation.location = [0,0];
-    //     $scope.userLocation.location[1] = newLat;
-    // }, true);
-    // $scope.$watch('userLocation._lng', function(newLng) {
-    //     if (!$scope.obs) return;
-    //     if (!newLng) return;
-    //     newLng = parseFloat(newLng);
-    //     if (isNaN(newLng)) newLng = 0;
-    //     if (newLng > 180) newLng = 180;
-    //     else if (newLng < -180) newLng = -180;
-    //     if (!$scope.userLocation.location) $scope.userLocation.location = [0,0];
-    //     $scope.userLocation.location[0] = newLng;
-    // }, true);
 
     // SELECT LOCATION MODAL
-
     $scope.selectLocation = function() {
-
         LocationSelectModal.open({
             initialLocation: $scope.user.location
         }).then(function (location) {
             if (location && location.length == 2) {
                 location[0] = parseFloat(location[0].toFixed(7)); 
                 location[1] = parseFloat(location[1].toFixed(7)); 
-
                 $scope.user.location = location;
-
-                // var featureCodes = 
-                //     "&featureCode=MT" +
-                //     "&featureCode=BUTE" +
-                //     "&featureCode=CNYN" +
-                //     "&featureCode=CONE" +
-                //     "&featureCode=CRQ" +
-                //     "&featureCode=CRQS" +
-                //     "&featureCode=DVD" +
-                //     "&featureCode=GAP" +
-                //     "&featureCode=GRGE" +
-                //     "&featureCode=HLL" +
-                //     "&featureCode=HLLS" +
-                //     "&featureCode=MESA" +
-                //     "&featureCode=MND" +
-                //     "&featureCode=MRN" +
-                //     "&featureCode=MT" +
-                //     "&featureCode=MTS" +
-                //     "&featureCode=NTK" +
-                //     "&featureCode=NTKS" +
-                //     "&featureCode=PASS" +
-                //     "&featureCode=PK" +
-                //     "&featureCode=PKS" +
-                //     "&featureCode=RDGE" +
-                //     "&featureCode=RK" +
-                //     "&featureCode=RKS" +
-                //     "&featureCode=SLP" +
-                //     "&featureCode=SPUR" +
-                //     "&featureCode=VAL" +
-                //     "&featureCode=VALG" +
-                //     "&featureCode=VALS" +
-                //     "&featureCode=VALX" +
-                //     "&featureCode=VLC";
-
-
-                // $.getJSON("https://ba-secure.geonames.net/findNearbyJSON?lat=" + location[1] + "&lng=" + location[0] + "" + featureCodes + "&username=avatech")
-                // .success(function(data, status, headers, config) {
-                //     if (data.geonames && data.geonames.length == 1) {
-                //         var place = data.geonames[0];
-                //         var name = "";
-                //         if (place.name) name += place.name;
-                //         // if US, append admin area code, otherwise append admin area name
-                //         if (place.countryCode && place.countryCode == "US" && place.adminCode1) name += ", " + place.adminCode1;
-                //         else if (place.adminName1) name += ", " + place.adminName1;
-
-                //         //$scope.obs.locationName = name;
-                //         //if (place.countryCode) $scope.obs.country = place.countryCode;
-
-                //         $scope.$apply();
-                //     }
-                // });
-                // $.getJSON("https://ba-secure.geonames.net/srtm3JSON?lat=" + location[1] + "&lng=" + location[0] + "&username=avatech")
-                // .success(function(data, status, headers, config) {
-                //     if (data.srtm3) {
-                //         var elevation = data.srtm3.toFixed(0);
-                //         // check for 'empty' value of -32768 (http://glcfapp.glcf.umd.edu/data/srtm/questions.shtml#negative)
-                //         if (elevation != -32768) {
-                //             $scope.obs.elevation = elevation; // meters
-                //             $scope.$apply();
-                //         }
-                //         // if no data found, check astergdem
-                //         else {
-                //             $.getJSON("https://ba-secure.geonames.net/astergdemJSON?lat=" + location[1] + "&lng=" + location[0] + "&username=avatech")
-                //             .success(function(data, status, headers, config) {
-                //                 if (data.astergdem) {
-                //                     var elevation = data.astergdem.toFixed(0);
-                //                     // check for 'empty' value of -9999 
-                //                     if (elevation != -9999){
-                //                         $scope.obs.elevation = elevation;
-                //                         $scope.$apply();
-                //                     }
-                //                 }
-                //             });
-                //         }
-                //     }
-                // });
             }
-        }, function () {
-            // on dismiss
         });
     }
 
@@ -220,7 +117,6 @@ function ($scope, $q, $stateParams, $location, $timeout, Global, Restangular, $h
             $scope.scrollToErrorPassword();
         }
         else {
-            //$http.post('/v1/users/' + $scope.htt, $scope.changePassword)
             $scope.user.customPOST($scope.changePassword, 'password')
             // success
             .then(function (data) {

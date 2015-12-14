1291a841f078ba7b33c7449e0cec2cf68291d4f0
Andrew Sohn <andrewsohn@gmail.com>
2015-12-14 09:54:42 -0700

    cleanup

--- public/modules/user/login.js
+++ public/modules/user/login.js
@@ -4,15 +4,7 @@ function ($scope, $rootScope, $timeout, $location, $http, Global, Restangular) {
     $scope.busy = false;
 
     $scope.submit = function() {
-
         // browser autofill hack
-        // todo: this doesn't seem to work in every case
-        // todo: new way of doing it? just set $scope.email and password by jQuery value...
-        // $("[ng-model]").each(function(index,el){
-        //     var ngModel = angular.element(el).data('$ngModelController');
-        //     var val = $(el).val();
-        //     ngModel.$setViewValue(val);
-        // });
         $scope.email = $(".signin #email").val();
         $scope.password = $(".signin #password").val();
 

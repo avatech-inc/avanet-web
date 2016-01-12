e1391e0fa6f3a962b17fe023cd8ab646135005b5
Andrew Sohn <andrewsohn@gmail.com>
2016-01-11 23:24:47 -0700

    cleanup

--- server/routes.js
+++ server/routes.js
@@ -13,19 +13,13 @@ module.exports = function(app) {
       , secret: 'VgUlNeUB02Q0pgsflT+p3/vDY9LdsXLOx/4IIONk'
       , bucket: 'avatech-uploads'
     });
-
     app.post('/upload', function(req, res) {
-        console.log("UPLAODING!");
-
         var form = new multiparty.Form();
-     
         form.parse(req, function(err, fields, files) {
             var file = files.fileData[0];
             if (!file) return;
-
             var fileName = uuid.v1().toString().replace(/-/g, "");
             fileName = '/snowpit-files/' + fileName + ".jpg";
-
             // upload to s3
             s3.putFile(file.path, fileName,
                 { 'Content-Type': 'image/jpeg', 'x-amz-acl': 'public-read' }, function(err, _res) {
@@ -34,9 +28,7 @@ module.exports = function(app) {
                 _res.resume();
                 res.json({ url: "https://s3.amazonaws.com/avatech-uploads" + fileName });
             });
-
         });
-
     });
 
     app.get('/download-app', function(req,res) { 

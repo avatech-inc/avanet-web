b05b954afee6b307e292ce3156846619c6b830fe
Peter Crosby <petercrosby@Macintosh.local>
2016-03-30 16:14:29 -0600

    changed PBF source to AWS bucket

--- src/modules/map/terrain-config/terrain-sources.js
+++ src/modules/map/terrain-config/terrain-sources.js
@@ -2,5 +2,5 @@ export default {
     pngV1: 'https://tiles-{s}.avatech.com/{z}/{x}/{y}.png',
     // pngV1: '',
     // pbfV2: 'http://73.228.98.110:8080/pbf/ut_esa_tiles_v6/{z}/{x}/{y}_ESA.pbf'
-    pbfV2: 'http://localhost:8888/pbf/ak10test_esa_tiles_v2/{z}/{x}/{y}_ESA.pbf'
+    pbfV2: 'https://s3.amazonaws.com/avatech-pbf-tiles/{z}/{x}/{y}_ESA.pbf'
 }
